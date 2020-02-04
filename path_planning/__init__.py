8f0a516473c9f9ad7143331a282ae39e17c00eaf
tsturzl <tsturzl@lefthandrobotics.com>
2020-02-03 17:24:28 -0700

    async wrapper doesn't work, cause GIL

--- path_planning/__init__.py
+++ path_planning/__init__.py
@@ -1,3 +1,5 @@
 from .path_planning import SpaceConf, RobotConf, RRTDubinsPlanner, create_circle
+import concurrent.futures
+import asyncio
 
 __all__ = ["SpaceConf", "RobotConf", "RRTDubinsPlanner", "create_circle"]
