c58c162e5e543e728ff6efae34b67a728963912d
tsturzl <tsturzl@lefthandrobotics.com>
2020-02-07 10:22:22 -0700

    take that GIL

--- path_planning/__init__.py
+++ path_planning/__init__.py
@@ -5,9 +5,10 @@ import asyncio
 __all__ = ["SpaceConf", "RobotConf", "RRTDubinsPlanner", "create_circle"]
 
 
-async def plan_async(planner):
+async def plan_async(planner, poll_interval=0.1):
     future = planner.plan()
     while True:
         n = future.check()
         if future.is_done(n):
             return future.finalize(n)
+        await asyncio.sleep(poll_interval)
